f17f997eac097c01282ce5ad056c1a86926ef65e
Manuel Ortega <mortega@apgca.cl>
2022-10-13 16:33:08 -0400

    initialize repository

---
+++ store/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { createWrapper } from "next-redux-wrapper";
+import rootReducer from "./rootReducer";
+
+// initial states here
+const initalState = {};
+
+// middleware
+const middleware = [thunk];
+
+// creating store
+export const store = configureStore({ reducer: rootReducer });
+
+// assigning store to next wrapper
+const makeStore = () => store;
+
+export const wrapper = createWrapper(makeStore);
